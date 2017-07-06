0768da1622cbad307c33138e98364711919a11f9
charles rymal <charlesrymal@gmail.com>
2017-07-06 14:09:49 -0700

    test callbacks async

--- async_patterns/tests/test_callbacks.py
+++ async_patterns/tests/test_callbacks.py
@@ -1,7 +1,12 @@
+import pytest
 
 from async_patterns import Callbacks
 
-def test_call():
+@pytest.fixture
+def loop():
+    return asyncio.get_event_loop()
+
+def test():
     cb = Callbacks()
     
     l = []
@@ -16,6 +21,24 @@ def test_call():
     cb.add_callback(func2)
     
     cb()
+
+    assert l == [1, 2]
+
+def test_async(loop):
+    cb = Callbacks()
+    
+    l = []
+
+    async def func1():
+        l.append(1)
+    
+    async def func2():
+        l.append(2)
+    
+    cb.add_callback(func1)
+    cb.add_callback(func2)
+    
+    loop.run_until_complete(cb.acall())
 
     assert l == [1, 2]
 
