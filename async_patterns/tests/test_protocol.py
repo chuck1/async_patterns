078e4fece89a5f31b944a020f8c90949a10a9f95
charles rymal <charlesrymal@gmail.com>
2017-07-07 08:51:29 -0700

    fill in protocol test

--- async_patterns/tests/test_protocol.py
+++ async_patterns/tests/test_protocol.py
@@ -6,21 +6,32 @@ import async_patterns.protocol
 class Packet:
     pass
 
+sp = None
+
 class ServerProtocol(async_patterns.protocol.Protocol):
-    pass
+    def __init__(self, loop):
+        global sp
+        print(self.__class__)
+        super(ServerProtocol, self).__init__(loop)
+        sp = self
 
 class ClientProtocol(async_patterns.protocol.Protocol):
-    pass
+    def __init__(self, loop):
+        print(self.__class__)
+        super(ClientProtocol, self).__init__(loop)
 
 async def atest(loop):
     server = await loop.create_server(functools.partial(ServerProtocol, loop), port=0)
     port = server.sockets[0].getsockname()[1]
     
-    await asyncio.sleep(2)
-
+    print('wait for server start')
+    await asyncio.sleep(4)
+    
     _, client = await loop.create_connection(functools.partial(ClientProtocol, loop), 'localhost', port)
     
     client.write(Packet)
+    
+    await sp.close()
 
     server.close()
     await server.wait_closed()
