d1a1d28711eec963e2414ba9c84bd93eb1150341
charles rymal <charlesrymal@gmail.com>
2017-07-07 15:29:44 -0700

    minor

--- async_patterns/__init__.py
+++ async_patterns/__init__.py
@@ -1,6 +1,9 @@
 __version__ = '0.3b4'
 
 from .callbacks import *
+from .coro_queue import *
+from . import callbacks
+from . import coro_queue
 
-__all__ = callbacks.__all__ + ['coro_queue', 'protocol']
+__all__ = callbacks.__all__ + coro_queue.__all__ + ['protocol']
 
