735a85bdd77f738d9b7809448d9322a595bdb2d2
charles rymal <charlesrymal@gmail.com>
2017-07-07 08:17:32 -0700

    add test_protocol

---
+++ async_patterns/tests/test_protocol.py
@@ -0,0 +1,28 @@
+import asyncio
+import functools
+
+import async_patterns.protocol
+
+class Packet:
+    pass
+
+class ServerProtocol(async_patterns.protocol.Protocol):
+    pass
+
+class ClientProtocol(async_patterns.protocol.Protocol):
+    pass
+
+async def atest(loop):
+    server = await loop.create_server(functools.partial(ServerProtocol, loop), port=0)
+    port = server.sockets[0].getsockname()[1]
+    
+    await asyncio.sleep(2)
+
+    _, client = await loop.create_connection(functools.partial(ClientProtocol, loop), 'localhost', port)
+    
+    server.close()
+    await server.wait_closed()
+
+def test(loop):
+    loop.run_until_complete(atest(loop))
+
