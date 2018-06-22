c25b4253e2df6368cbfc25e1adff222831630c8b
Charles Rymal <charlesrymal@gmail.com>
2018-06-22 18:38:55 +0000

    add AsyncSortedGenerators

--- async_patterns/sorted_generators.py
+++ async_patterns/sorted_generators.py
@@ -117,6 +117,132 @@ class SortedGeneratorsIterator:
 
         return self.__iterators[0].__next__()
 
+#######################################################################
+
+
+class AsyncSortedGenerator:
+    def __init__(self, generator, sort_key, reverse=False):
+        self.__generator = generator
+        self.sort_key = sort_key
+        self.reverse = reverse
+
+    def __aiter__(self):
+        return AsyncSortedGeneratorIterator(self.__generator.__aiter__(), self.sort_key)
+ 
+class AsyncSortedGeneratorIterator:
+    def __init__(self, iterator, sort_key, reverse=False):
+        self.__iterator = iterator
+
+        self._ready = False
+        self._stop = False
+        
+        self.sort_key = sort_key
+        self.reverse = reverse
+
+
+    def peek(self):
+        assert self._ready
+        return self.__next
+
+    def peek_sort_key(self):
+        assert self._ready
+        return self.sort_key(self.__next)
+
+    async def _first(self):
+        if self._ready: return
+        
+        try:
+            self.__next = await self.__iterator.__anext__()
+        except StopAsyncIteration:
+            self._stop = True
+
+        self._ready = True
+
+    async def __anext__(self):
+        if self._stop: raise StopAsyncIteration()
+        
+        _ = self.__next
+
+        try:
+            self.__next = await self.__iterator.__anext__()
+        except StopAsyncIteration:
+            self._stop = True
+
+        return _
+
+    def __eq__(self, other):
+        if self._stop != other._stop: return False
+        return self.peek_sort_key() == other.peek_sort_key()
+
+    def __lt__(self, other):
+        if self._stop and other._stop: return False
+        if self._stop == other._stop:
+            return self.peek_sort_key() < other.peek_sort_key()
+        if self._stop:
+            return True
+        else:
+            return False
+
+    def __le__(self, other):
+        if self._stop and other._stop: return True
+
+        if self._stop == other._stop:
+            return self.peek_sort_key() <= other.peek_sort_key()
+        if self._stop:
+            return True
+        else:
+            return False
+
+    def __gt__(self, other):
+        if self._stop and other._stop: return False
+
+        if self._stop == other._stop:
+            return self.peek_sort_key() > other.peek_sort_key()
+        if self._stop:
+            return False
+        else:
+            return True
+        
+    def __ge__(self, other):
+        if self._stop and other._stop: return True
+
+        if self._stop == other._stop:
+            return self.peek_sort_key() >= other.peek_sort_key()
+        if self._stop:
+            return False
+        else:
+            return True
+
+class AsyncSortedGenerators:
+    def __init__(self, generators, sort_key, reverse=False):
+        self.generators = [AsyncSortedGenerator(_, sort_key) for _ in generators]
+
+        self.sort_key = lambda it: sort_key(it.peek())
+        self.reverse = reverse
+
+    def __aiter__(self):
+        return AsyncSortedGeneratorsIterator(
+                [_.__aiter__() for _ in self.generators],
+                self.reverse)
+
+class AsyncSortedGeneratorsIterator:
+    def __init__(self, iterators, reverse=False):
+        self.__iterators = iterators
+        self.reverse = reverse
+
+    async def __anext__(self):
+        for i in self.__iterators:
+            await i._first()
+
+        self.__iterators = sorted(self.__iterators, reverse=self.reverse)
+
+        while self.__iterators and self.__iterators[0]._stop:
+            self.__iterators.pop(0)
+
+        if not self.__iterators: raise StopAsyncIteration()
+
+        return await self.__iterators[0].__anext__()
+
 
 
 
--- async_patterns/tests/test_sorted_generator.py
+++ async_patterns/tests/test_sorted_generator.py
@@ -1,7 +1,9 @@
 import functools
 import operator
 
-from async_patterns.sorted_generators import SortedGenerators
+import pytest
+
+from async_patterns.sorted_generators import SortedGenerators, AsyncSortedGenerators
 
 def test_0():
     print()
@@ -29,4 +31,30 @@ def test_1():
 
     assert(l0 == l1)
 
+@pytest.mark.asyncio
+async def test_2():
+
+    async def _f0():
+        for i in range(10, 20):
+            yield i
+
+    async def _f1():
+        for i in range(0, 30, 3):
+            yield i
+
+    s = AsyncSortedGenerators([_f0(), _f1()], lambda x: x)
+
+    l0 = [x async for x in s]
+
+    async def _y():
+        for _ in [_f0(), _f1()]:
+            async for x in _: yield x
+
+    l1 = sorted([x async for x in _y()])
+
+    print(l0)
+    print(l1)
+
+    assert(l0 == l1)
+   
 
