6d2a14803e10fc5f334268af0fd049cdcf66a3db
Charles Rymal <charlesrymal@gmail.com>
2018-09-19 19:22:32 +0000

    add aislice

--- async_patterns/__init__.py
+++ async_patterns/__init__.py
@@ -7,3 +7,24 @@ from . import coro_queue
 
 __all__ = callbacks.__all__ + coro_queue.__all__ + ['protocol']
 
+async def aenumerate(aiterable):
+    i = 0
+    async for x in aiterable:
+        yield i, x
+        i += 1
+
+async def aislice(aiterable, *args):
+    s = slice(*args)
+    it = iter(range(s.start or 0, s.stop or sys.maxsize, s.step or 1))
+    try:
+        nexti = next(it)
+    except StopIteration:
+        return
+    async for i, element in aenumerate(aiterable):
+        if i == nexti:
+            yield element
+            try:
+                nexti = next(it)
+            except StopIteration:
+                return
+
